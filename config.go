f78f70d4661235bbd549e7464f7a95150165fb77
kiwiidb <kwinten.debacker@hotmail.com>
2022-07-18 17:06:09 +0200

    configure expiry

--- config.go
+++ config.go
@@ -1,10 +1,12 @@
 package main
 
 type Config struct {
-	Port        int    `default:"8081"`
-	JWTSecret   []byte `envconfig:"JWT_SECRET" required:"true"`
-	DatabaseUri string `envconfig:"DATABASE_URI" required:"true"`
-	LndHubUrl   string `envconfig:"LNDHUB_URL" required:"true"`
-	TargetFile  string `envconfig:"TARGET_FILE" default:"targets.json"`
-	SentryDSN   string `envconfig:"SENTRY_DSN"`
+	Port                   int    `default:"8081"`
+	JWTSecret              []byte `envconfig:"JWT_SECRET" required:"true"`
+	DatabaseUri            string `envconfig:"DATABASE_URI" required:"true"`
+	LndHubUrl              string `envconfig:"LNDHUB_URL" required:"true"`
+	TargetFile             string `envconfig:"TARGET_FILE" default:"targets.json"`
+	SentryDSN              string `envconfig:"SENTRY_DSN"`
+	AccessTokenExpSeconds  int    `default:"7200"`    //default 2 hours
+	RefreshTokenExpSeconds int    `default:"2592000"` //default 30 days
 }
