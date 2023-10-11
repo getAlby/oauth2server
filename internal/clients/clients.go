ae8f93fd6888c0ee73e24418d03deab97a39ab01
kiwiidb <kwinten.debacker@hotmail.com>
2023-10-11 17:14:19 +0200

    start refactoring clients pkg

---
+++ internal/clients/clients.go
@@ -0,0 +1,191 @@
+package clients
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"oauth2server/constants"
+	"oauth2server/models"
+
+	"github.com/getsentry/sentry-go"
+	"github.com/go-playground/validator"
+	"gorm.io/gorm"
+
+	"github.com/gorilla/mux"
+	"github.com/labstack/gommon/random"
+	"github.com/sirupsen/logrus"
+)
+
+type service struct {
+	cs ClientStore
+}
+
+type ClientStore interface {
+	Create(ctx context.Context, id, secret, domain, url, imageUrl, name string) error
+	ListAllClients() (result []models.ClientMetaData, err error)
+}
+
+func NewService(cs ClientStore) *service {
+	return &service{
+		cs: cs,
+	}
+}
+
+func RegisterRoutes(r *mux.Router, svc *service) {
+	//these routes should not be publicly accesible
+	r.HandleFunc("/admin/clients", svc.CreateClientHandler).Methods(http.MethodPost)
+	r.HandleFunc("/admin/clients", svc.ListAllClientsHandler).Methods(http.MethodGet)
+	r.HandleFunc("/admin/clients/{clientId}", svc.FetchClientHandler).Methods(http.MethodGet)
+	r.HandleFunc("/admin/clients/{clientId}", svc.UpdateClientMetadataHandler).Methods(http.MethodPut)
+}
+
+func (svc *service) CreateClientHandler(w http.ResponseWriter, r *http.Request) {
+	req := &models.CreateClientRequest{}
+	err := json.NewDecoder(r.Body).Decode(req)
+	if err != nil {
+		logrus.Errorf("Error decoding client info request %s", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		_, err = w.Write([]byte("Could not parse create client request"))
+		if err != nil {
+			logrus.Error(err)
+		}
+		return
+	}
+	err = validator.New().Struct(req)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_, err = w.Write([]byte(err.Error()))
+		if err != nil {
+			logrus.Error(err)
+		}
+		return
+	}
+	id := random.New().String(constants.ClientIdLength)
+	var secret string
+	if !req.Public {
+		secret = random.New().String(constants.ClientSecretLength)
+	}
+
+	err = svc.cs.Create(r.Context(), id, secret, req.Domain, req.URL, req.ImageUrl, req.Name)
+	if err != nil {
+		logrus.Errorf("Error storing client info %s", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		_, err = w.Write([]byte("Something went wrong while storing client info"))
+		if err != nil {
+			logrus.Error(err)
+		}
+	}
+
+	w.Header().Add("Content-type", "application/json")
+	err = json.NewEncoder(w).Encode(&models.CreateClientResponse{
+		Name:         req.Name,
+		ImageUrl:     req.ImageUrl,
+		ClientId:     id,
+		ClientSecret: secret,
+	})
+	if err != nil {
+		logrus.Error(err)
+	}
+}
+
+func (svc *service) ListAllClientsHandler(w http.ResponseWriter, r *http.Request) {
+	result, err := svc.cs.ListAllClients()
+	if err != nil {
+		sentry.CaptureException(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	response := []models.ListClientsResponse{}
+	for _, md := range result {
+		response = append(response, models.ListClientsResponse{
+			ID:       md.ClientID,
+			Name:     md.Name,
+			ImageURL: md.ImageUrl,
+			URL:      md.URL,
+		})
+	}
+	w.Header().Add("Content-type", "application/json")
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		logrus.Error(err)
+	}
+}
+
+func (svc *service) UpdateClientMetadataHandler(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["clientId"]
+	req := &models.CreateClientRequest{}
+	err := json.NewDecoder(r.Body).Decode(req)
+	if err != nil {
+		logrus.Errorf("Error decoding client info request %s", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		_, err = w.Write([]byte("Could not parse create client request"))
+		if err != nil {
+			logrus.Error(err)
+		}
+		return
+	}
+	found := &models.ClientMetaData{}
+	err = svc.DB.FirstOrCreate(found, &models.ClientMetaData{ClientID: id}).Error
+	if err != nil {
+		logrus.Errorf("Error storing client info %s", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		_, err = w.Write([]byte("Something went wrong while storing client info"))
+		if err != nil {
+			logrus.Error(err)
+		}
+		return
+	}
+	if req.Name != "" {
+		found.Name = req.Name
+	}
+	if req.ImageUrl != "" {
+		found.ImageUrl = req.ImageUrl
+	}
+	if req.URL != "" {
+		found.URL = req.URL
+	}
+	err = svc.DB.Save(found).Error
+	if err != nil {
+		logrus.Errorf("Error storing client info %s", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		_, err = w.Write([]byte("Something went wrong while storing client info"))
+		if err != nil {
+			logrus.Error(err)
+		}
+		return
+	}
+	w.Header().Add("Content-type", "application/json")
+	err = json.NewEncoder(w).Encode(&models.CreateClientResponse{
+		ClientId: id,
+		Name:     req.Name,
+		ImageUrl: req.ImageUrl,
+		Url:      req.URL,
+	})
+	if err != nil {
+		logrus.Error(err)
+	}
+}
+func (service *service) FetchClientHandler(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["clientId"]
+	result := models.ClientMetaData{}
+	err := service.DB.First(&result, &models.ClientMetaData{ClientID: id}).Error
+	if err != nil {
+		sentry.CaptureException(err)
+		status := http.StatusInternalServerError
+		if err == gorm.ErrRecordNotFound {
+			status = http.StatusNotFound
+		}
+		http.Error(w, err.Error(), status)
+		return
+	}
+	w.Header().Add("Content-type", "application/json")
+	err = json.NewEncoder(w).Encode(&models.ListClientsResponse{
+		ID:       result.ClientID,
+		Name:     result.Name,
+		ImageURL: result.ImageUrl,
+		URL:      result.URL,
+	})
+	if err != nil {
+		logrus.Error(err)
+	}
+}
---
+++ internal/clients/repository_gorm.go
@@ -0,0 +1,49 @@
+package clients
+
+import (
+	"context"
+	"oauth2server/models"
+
+	oauth2gorm "github.com/getAlby/go-oauth2-gorm"
+	mdls "github.com/go-oauth2/oauth2/v4/models"
+	"gorm.io/gorm"
+)
+
+type gormClientStore struct {
+	db *gorm.DB
+	cs *oauth2gorm.ClientStore
+}
+
+// ListAllClients implements ClientStore.
+func (store *gormClientStore) ListAllClients() (result []models.ClientMetaData, err error) {
+	result = []models.ClientMetaData{}
+	err = store.db.Find(&result, &models.ClientMetaData{}).Error
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (store *gormClientStore) Create(ctx context.Context, id string, secret string, domain string, url string, imageUrl, name string) error {
+	err := store.cs.Create(ctx, &mdls.Client{
+		ID:     id,
+		Secret: secret,
+		Domain: domain,
+	})
+	if err != nil {
+		return err
+	}
+	return store.db.Create(&models.ClientMetaData{
+		ClientID: id,
+		Name:     name,
+		ImageUrl: imageUrl,
+		URL:      url,
+	}).Error
+}
+
+func NewGormClientStore(db *gorm.DB, cs *oauth2gorm.ClientStore) ClientStore {
+	return &gormClientStore{
+		db: db,
+		cs: cs,
+	}
+}
