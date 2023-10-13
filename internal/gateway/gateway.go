b07b0229514eba7bd6571537780dce1a5ec7d655
kiwiidb <kwinten.debacker@hotmail.com>
2023-10-13 16:41:32 +0200

    refactoring almost done

---
+++ internal/gateway/gateway.go
@@ -0,0 +1,93 @@
+package gateway
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"oauth2server/models"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/go-oauth2/oauth2/v4"
+	"github.com/golang-jwt/jwt"
+)
+
+func InitGateways(targetFile string,
+	tokenFunc func(context.Context, string) (oauth2.TokenInfo, error),
+	jwtSecret []byte,
+) (result []*OriginServer, err error) {
+	targetBytes, err := os.ReadFile(targetFile)
+	if err != nil {
+		return nil, err
+	}
+	result = []*OriginServer{}
+	err = json.Unmarshal(targetBytes, &result)
+	if err != nil {
+		return nil, err
+	}
+	originHelperMap := map[string]http.Handler{}
+	for _, origin := range result {
+		origin.JWTInjectFunc = func(ti oauth2.TokenInfo, r *http.Request) error {
+			return InjectJWTAccessToken(ti, r, jwtSecret)
+		}
+		origin.CheckTokenFunc = tokenFunc
+		//avoid creating too much identical origin server objects
+		//by storing them in a map
+		value, found := originHelperMap[origin.Origin]
+		if found {
+			//use existing one
+			origin.proxy = value
+		} else {
+			//create new one
+			originUrl, err := url.Parse(origin.Origin)
+			if err != nil {
+				return nil, err
+			}
+			proxy := httputil.NewSingleHostReverseProxy(originUrl)
+			originHelperMap[origin.Origin] = proxy
+			origin.proxy = proxy
+		}
+	}
+	return result, nil
+}
+
+func InjectJWTAccessToken(token oauth2.TokenInfo, r *http.Request, secret []byte) error {
+	//mint and inject jwt token needed for origin server
+	//the request is dispatched immediately, so the tokens can have a short expiry
+	expirySeconds := 60
+	lndhubId := token.GetUserID()
+	lndhubToken, err := generateLNDHubAccessToken(secret, expirySeconds, lndhubId)
+	if err != nil {
+		return err
+	}
+	r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", lndhubToken))
+	return nil
+}
+
+// GenerateAccessToken : Generate Access Token
+func generateLNDHubAccessToken(secret []byte, expiryInSeconds int, userId string) (string, error) {
+	//convert string to int
+	id, err := strconv.Atoi(userId)
+	if err != nil {
+		return "", err
+	}
+	claims := &models.LNDhubClaims{
+		ID: int64(id),
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Second * time.Duration(expiryInSeconds)).Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	t, err := token.SignedString(secret)
+	if err != nil {
+		return "", err
+	}
+
+	return t, nil
+}
---
+++ internal/gateway/gateway_test.go
@@ -0,0 +1,74 @@
+package gateway
+
+/*
+TODO
+func TestGateway(t *testing.T) {
+	//init test origin server at localhost:8082
+	//make a channel to intercept the jwt token
+	jwtChan := make(chan string, 1)
+	originServerMsg := "Hi, you have reached the origin server"
+	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		jwtChan <- r.Header.Get("Authorization")
+		_, err := w.Write([]byte(originServerMsg))
+		assert.NoError(t, err)
+	}))
+	l, _ := net.Listen("tcp", "localhost:8082")
+	ts.Listener = l
+	ts.Start()
+	defer ts.Close()
+
+	svc, controller := initService(t)
+	gateways, err := svc.InitGateways()
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(gateways))
+	cli, err := createClient(controller, &testClient)
+	assert.NoError(t, err)
+	rec, err := fetchCode(cli.ClientId, testClient.Domain, "balance:read", controller)
+	assert.NoError(t, err)
+	loc := rec.Header().Get("Location")
+	redirect, err := url.Parse(loc)
+	assert.NoError(t, err)
+	code := redirect.Query().Get("code")
+	assert.NotEmpty(t, code)
+	rec, err = fetchToken(cli.ClientId, cli.ClientSecret, code, testClient.Domain, controller)
+	assert.NoError(t, err)
+	resp := &TokenResponse{}
+	err = json.NewDecoder(rec.Body).Decode(resp)
+	assert.NoError(t, err)
+	//make API request
+	req, err := http.NewRequest(http.MethodGet, "/balance", nil)
+	req.Header.Set("Authorization", resp.AccessToken)
+	assert.NoError(t, err)
+	rec = httptest.NewRecorder()
+	//wrap gateway with middleware
+	gw1 := middleware.RegisterMiddleware(gateways[0], svc.Config)
+	//we're not testing the gateway selection logic at the moment
+	gw1.ServeHTTP(rec, req)
+	//assert that we get a response from the right backend
+	assert.Equal(t, originServerMsg, rec.Body.String())
+	//check backend server that we got a jwt token
+	token := <-jwtChan
+	assert.Contains(t, token, "Bearer ")
+	claims := jwt.MapClaims{}
+	jwtToken := strings.TrimPrefix(token, "Bearer ")
+	parsed, err := jwt.ParseWithClaims(jwtToken, claims, func(token *jwt.Token) (interface{}, error) {
+		return svc.Config.JWTSecret, nil
+	})
+	assert.NoError(t, err)
+	assert.True(t, parsed.Valid)
+	assert.Positive(t, claims["id"])
+	assert.False(t, claims["isRefresh"].(bool))
+
+	//make request with token for wrong scope, assert that this fails
+	req, err = http.NewRequest(http.MethodGet, "/invoices/incoming", nil)
+	assert.NoError(t, err)
+	req.Header.Set("Authorization", resp.AccessToken)
+	assert.NoError(t, err)
+	rec = httptest.NewRecorder()
+	//wrap gateway with mw
+	gw2 := middleware.RegisterMiddleware(gateways[1], svc.Config)
+	gw2.ServeHTTP(rec, req)
+	assert.Equal(t, http.StatusUnauthorized, rec.Result().StatusCode)
+}
+
+*/
