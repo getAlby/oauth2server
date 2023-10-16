11739661b816e5c262285b99031d603f25a69c2d
kiwiidb <kwinten.debacker@hotmail.com>
2023-10-16 10:34:48 +0200

    fix gateway test

--- internal/gateway/gateway_test.go
+++ internal/gateway/gateway_test.go
@@ -1,7 +1,51 @@
 package gateway
 
-/*
-TODO
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/go-oauth2/oauth2/v4"
+	"github.com/go-oauth2/oauth2/v4/models"
+	"github.com/golang-jwt/jwt"
+	"github.com/stretchr/testify/assert"
+)
+
+const testToken = "TEST1234"
+
+var testJWTSecret = []byte("SUPERSECRET")
+
+type mockToken struct{}
+
+func testTokenFunc(ctx context.Context, token string) (result oauth2.TokenInfo, err error) {
+
+	if token != testToken {
+		return result, fmt.Errorf("unauthorized")
+	}
+	return &models.Token{
+		ClientID:            "TEST",
+		UserID:              "123",
+		RedirectURI:         "",
+		Scope:               "balance:read",
+		Code:                "",
+		CodeChallenge:       "",
+		CodeChallengeMethod: "",
+		CodeCreateAt:        time.Time{},
+		CodeExpiresIn:       0,
+		Access:              testToken,
+		AccessCreateAt:      time.Time{},
+		AccessExpiresIn:     0,
+		Refresh:             "",
+		RefreshCreateAt:     time.Time{},
+		RefreshExpiresIn:    0,
+	}, nil
+}
+
 func TestGateway(t *testing.T) {
 	//init test origin server at localhost:8082
 	//make a channel to intercept the jwt token
@@ -17,33 +61,18 @@ func TestGateway(t *testing.T) {
 	ts.Start()
 	defer ts.Close()
 
-	svc, controller := initService(t)
-	gateways, err := svc.InitGateways()
+	gateways, err := InitGateways("test_targets.json", testTokenFunc, testJWTSecret)
 	assert.NoError(t, err)
 	assert.Equal(t, 2, len(gateways))
-	cli, err := createClient(controller, &testClient)
-	assert.NoError(t, err)
-	rec, err := fetchCode(cli.ClientId, testClient.Domain, "balance:read", controller)
-	assert.NoError(t, err)
-	loc := rec.Header().Get("Location")
-	redirect, err := url.Parse(loc)
-	assert.NoError(t, err)
-	code := redirect.Query().Get("code")
-	assert.NotEmpty(t, code)
-	rec, err = fetchToken(cli.ClientId, cli.ClientSecret, code, testClient.Domain, controller)
-	assert.NoError(t, err)
-	resp := &TokenResponse{}
-	err = json.NewDecoder(rec.Body).Decode(resp)
-	assert.NoError(t, err)
 	//make API request
 	req, err := http.NewRequest(http.MethodGet, "/balance", nil)
-	req.Header.Set("Authorization", resp.AccessToken)
+	req.Header.Set("Authorization", testToken)
 	assert.NoError(t, err)
-	rec = httptest.NewRecorder()
+	rec := httptest.NewRecorder()
 	//wrap gateway with middleware
-	gw1 := middleware.RegisterMiddleware(gateways[0], svc.Config)
 	//we're not testing the gateway selection logic at the moment
-	gw1.ServeHTTP(rec, req)
+	gateways[0].ServeHTTP(rec, req)
+	assert.Equal(t, http.StatusOK, rec.Code)
 	//assert that we get a response from the right backend
 	assert.Equal(t, originServerMsg, rec.Body.String())
 	//check backend server that we got a jwt token
@@ -52,7 +81,7 @@ func TestGateway(t *testing.T) {
 	claims := jwt.MapClaims{}
 	jwtToken := strings.TrimPrefix(token, "Bearer ")
 	parsed, err := jwt.ParseWithClaims(jwtToken, claims, func(token *jwt.Token) (interface{}, error) {
-		return svc.Config.JWTSecret, nil
+		return testJWTSecret, nil
 	})
 	assert.NoError(t, err)
 	assert.True(t, parsed.Valid)
@@ -62,13 +91,11 @@ func TestGateway(t *testing.T) {
 	//make request with token for wrong scope, assert that this fails
 	req, err = http.NewRequest(http.MethodGet, "/invoices/incoming", nil)
 	assert.NoError(t, err)
-	req.Header.Set("Authorization", resp.AccessToken)
+	req.Header.Set("Authorization", testToken)
 	assert.NoError(t, err)
 	rec = httptest.NewRecorder()
 	//wrap gateway with mw
-	gw2 := middleware.RegisterMiddleware(gateways[1], svc.Config)
+	gw2 := gateways[1]
 	gw2.ServeHTTP(rec, req)
 	assert.Equal(t, http.StatusUnauthorized, rec.Result().StatusCode)
 }
-
-*/
